b0c4babc0c598e8e82c0bbe2bdeaae2d8e11b3f0
Adelina Ciorap <c.adelina@outlook.com>
2019-05-03 15:09:03 +0300

    1

---
+++ String_uniquechars/String_uniquechars/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_uniquechars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string: ");
+            string input = Console.ReadLine();
+            if (IsUnique(input) == true)
+                Console.WriteLine("The String " + input
+                                  + " has all unique characters");
+            else
+                Console.WriteLine("The String " + input
+                                  + " has duplicate characters");
+
+        }
+
+        private static bool IsUnique(string chars)
+
+        {
+            for (int i = 0; i < chars.Length; i++)
+            {
+                for (int j = i + 1; j < chars.Length; j++)
+                {
+                    int val = chars[i];
+                    if (chars[i] == chars[j])
+                    {
+                        return false;
+                    }
+
+                }
+
+
+            }
+            return true;
+
+        }
+    }
+}
